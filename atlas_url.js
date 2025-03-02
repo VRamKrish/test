ae69d2e30298b790c04619b631af3dcaeccbc67d
VRamKrish <161112274+VRamKrish@users.noreply.github.com>
2025-03-02 12:46:25 +0530

    Update atlas_url.js
    removed url

--- atlas_url.js
+++ atlas_url.js
@@ -1,3 +1,1 @@
-module.exports = url =
-  "mongodb+srv://vamsiram212:varakri212@cluster0.ddymg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-//   mongodb+srv://vamsiram212:<db_password>@cluster0.ddymg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+module.exports = url =""
