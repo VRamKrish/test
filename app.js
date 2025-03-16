97660dfd6ee11509f75edab52ea0ed1971356b97
VRamKrish <vamsiram212@gmail.com>
2025-03-16 13:43:56 +0530

    mongobd connection error fix

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-require('dotenv').config({ path: './.env.local' });
+require('dotenv').config({ path: './.env' });
 
 // app.listen(8080, () => {
 //   console.log("Server is running on port 8080");
@@ -15,6 +15,7 @@ app.use(express.json());
 // Middleware to parse URL-encoded data
 app.use(express.urlencoded({ extended: true }));
 // Connect to MongoDB
+console.log(process.env.MONGO_USER, process.env.MONGO_PWD);
 
 mongoose.connect(
   `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PWD}@cluster0.ddymg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
